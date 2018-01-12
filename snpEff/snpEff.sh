ede381ef8a165ec7a175c2a3e0e7df37d0783a3c
Washington Candeia de Araujo <araujowash@gmail.com>
2018-01-12 13:09:03 -0300

    snpEff script

---
+++ snpEff/snpEff.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH --job-name=Var
+#SBATCH --output=slurm%j.out
+#SBATCH --error=slurm%j.err 
+#SBATCH --nodes=4
+##SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=32
+#SBATCH --hint=compute_bound
+#SBATCH --time=7-0:0
+
+set -euo pipefail
+
+VARIANT=/scratch/global/wcdaraujo/exome/bwasw/variantCalling/
+
+# snpEff - Preditor
+java -jar /home/wcdaraujo/snpEff/snpEff.jar eff -c /home/wcdaraujo/snpEff/snpEff.config GRCh38.86 ${VARIANT}1_sample.vcf > ${VARIANT}1_sample.eff.vcf
+java -jar /home/wcdaraujo/snpEff/snpEff.jar eff -c /home/wcdaraujo/snpEff/snpEff.config GRCh38.86 ${VARIANT}3_sample.vcf > ${VARIANT}3_sample.eff.vcf
+java -jar /home/wcdaraujo/snpEff/snpEff.jar eff -c /home/wcdaraujo/snpEff/snpEff.config GRCh38.86 ${VARIANT}4_sample.vcf > ${VARIANT}4_sample.eff.vcf
+java -jar /home/wcdaraujo/snpEff/snpEff.jar eff -c /home/wcdaraujo/snpEff/snpEff.config GRCh38.86 ${VARIANT}5_sample.vcf > ${VARIANT}5_sample.eff.vcf
+
