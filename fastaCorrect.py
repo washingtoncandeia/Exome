5550c65fedc08e752a2636b60dc28d5e8161a5ae
Washington Candeia de Araujo <araujowash@gmail.com>
2018-01-17 13:02:34 -0300

    Biopython Fasta

--- fastaCorrect.py
+++ fastaCorrect.py
@@ -4,5 +4,5 @@
 # Importante ao usar prinseq
 
 from Bio import SeqIO
-SeqIO.convert("Homo_sapiens.GRCh37.75.dna_split.fa", "fasta", "Homo_sapiens.GRCh37.75.dna_split.fa", "fasta")
+SeqIO.convert("Homo_sapiens.GRCh37.75.dna_split.fa", "fasta", "Homo_sapiens.GRCh37.75.fa", "fasta")
 
