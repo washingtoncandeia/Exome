a3fa8a785c03dd1ad65cdff7394e40e9d3e035db
Washington Candeia de Araujo <araujowash@gmail.com>
2018-01-23 15:18:33 -0300

    Arquivo deletado

--- fastaCorrect.py
+++
@@ -1,8 +0,0 @@
-#!/bin/env python
-
-# Programa para corrigir arquivo fasta.
-# Importante ao usar prinseq
-
-from Bio import SeqIO
-SeqIO.convert("Homo_sapiens.GRCh37.75.dna_split.fa", "fasta", "Homo_sapiens.GRCh37.75.fa", "fasta")
-
