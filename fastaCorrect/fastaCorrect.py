fe54c302ea006fbf6a04541b36b59e57a1fcfa6a
Washington Candeia de Araujo <araujowash@gmail.com>
2018-01-23 15:19:05 -0300

    Biopython fasta correct

---
+++ fastaCorrect/fastaCorrect.py
@@ -0,0 +1,8 @@
+#!/bin/env python
+
+# Programa para corrigir arquivo fasta.
+# Importante ao usar prinseq
+
+from Bio import SeqIO
+SeqIO.convert("Homo_sapiens.GRCh37.75.dna_split.fa", "fasta", "Homo_sapiens.GRCh37.75.fa", "fasta")
+
